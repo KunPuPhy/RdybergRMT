cb28f4aa2bd1e39beb4ef93bbb77fc3ce3bcfacf
root <root@PC-20240906PIVU>
2025-01-03 22:23:53 +0800

    Add search_i function in Basis module and its tests file

--- rdybergrmt/Basis.py
+++ rdybergrmt/Basis.py
@@ -4,6 +4,39 @@ Basis
 This module contains the basis of the lattice.
 
 """
+__all__ = ['get_spin_basis', 'search_i']
+
+# get the idex of a basis
+def search_i(basis, i):
+    """
+    Search the idex of 'i' in the basis.
+
+    Parameters
+    ----------
+    basis : list
+        The basis of the lattice.
+    i : int
+        The element of the basis.
+    
+    Return
+    -------
+    idex : int
+        The idex of 'i' in the basis.
+        Starts from 0 and left to right.
+
+    """
+    L = len(basis)
+    if L == 0:
+        return False
+    
+    idex = 0
+    for temp in basis:
+        if temp == i:
+           break
+        idex = idex + 1
+        if idex >= L:
+            return False
+    return idex
 
 def get_spin_basis(L):
     """
--- tests/Basis_tests.py
+++ tests/Basis_tests.py
@@ -4,7 +4,69 @@ import unittest
 from rdybergrmt import *
 
 
+class TestSearchI(unittest.TestCase):
+    """
+    
+    Tests for the search_i function.
+
+    """
+    def test_search_i_ElementAtStart(self):
+        basis = [1, 2, 3, 4, 5]
+        i = 1
+        self.assertEqual(search_i(basis, i), 0)
+
+    def test_search_i_ElementInMiddle(self):
+        basis = [1, 2, 3, 4, 5]
+        i = 3
+        self.assertEqual(search_i(basis, i), 2)
+
+    def test_search_i_ElementAtEnd(self):
+        basis = [1, 2, 3, 4, 5]
+        i = 5
+        self.assertEqual(search_i(basis, i), 4)
+
+    def test_search_i_ElementNotFound(self):
+        basis = [1, 2, 3, 4, 5]
+        i = 7
+        self.assertEqual(search_i(basis, i), False)
+
+    def test_search_i_ElementLessThanMin(self):
+        basis = [1, 2, 3, 4, 5]
+        i = 0
+        self.assertEqual(search_i(basis, i), False)
+
+    def test_search_i_ElementGreaterThanMax(self):
+        basis = [1, 2, 3, 4, 5]
+        i = 6
+        self.assertEqual(search_i(basis, i), False)
+
+    def test_search_i_ElementNotInList(self):
+        basis = [1, 2, 4, 5]
+        i = 3
+        self.assertEqual(search_i(basis, i), False)
+
+    def test_search_i_EmptyBasis(self):
+        basis = []
+        i = 1
+        self.assertEqual(search_i(basis, i), False)
+
+    def test_search_i_SingleElementMatch(self):
+        basis = [1]
+        i = 1
+        self.assertEqual(search_i(basis, i), 0)
+
+    def test_search_i_SingleElementNoMatch(self):
+        basis = [1]
+        i = 2
+        self.assertEqual(search_i(basis, i), False)
+
+
 class TestGetSpinBasis(unittest.TestCase):
+    """
+    
+    Tests for the get_spin_basis function.
+    
+    """
     def test_get_spin_basis_L_0(self):
         self.assertEqual(get_spin_basis(0), [0])
 
